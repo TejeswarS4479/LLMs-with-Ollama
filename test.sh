e1698cd14d5510493865edff5514f51e11e1388d
Tejeswar Reddy Seerapu <tejeswar.seerapu@soulax.io>
2026-03-27 15:05:18 +0530

    Print API Token - testing push protection

---
+++ test.sh
@@ -0,0 +1,3 @@
+echo 'GITHUB_TOKEN=github_pat_11AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' > config.env
+git add config.env
+git commit -m "test: checking push protection"
